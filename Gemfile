3719a58d3de965a0dab6495dfec5941edcc82fd9
Drew Barontini <drewbarontini@gmail.com>
2013-10-17 06:48:30 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~>3.1.6"
+gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-minify-html"
+gem "builder"
---
+++ config.rb
@@ -0,0 +1,23 @@
+activate :automatic_image_sizes
+activate :directory_indexes
+activate :livereload
+activate :minify_html
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
